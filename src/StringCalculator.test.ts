735af8bce0baa6f414949148e956f21852dc4a90
Nitin Kukreti <nitinkukreti12@gmail.com>
2024-11-05 22:30:00 +0530

    Test: Support custom delimiters

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -26,6 +26,10 @@ describe('StringCalculator', () => {
     expect(calculator.add("1\n2,3")).toBe(6);
   });
   
+  it('should support custom delimiters', () => {
+    const calculator = new StringCalculator();
+    expect(calculator.add("//;\n1;2")).toBe(3);
+  });
   
 
 });
--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,7 +1,13 @@
 export class StringCalculator {
     add(numbers: string): number {
         if (numbers === "") return 0;
-        const numArray = numbers.split(/,|\n/).map(Number);
+        let delimiter = /,|\n/;
+        if (numbers.startsWith("//")) {
+            const parts = numbers.split('\n');
+            delimiter = new RegExp(parts[0][2]);
+            numbers = parts[1];
+        }
+        const numArray = numbers.split(delimiter).map(Number);
         return numArray.reduce((sum, num) => sum + num, 0);
       }
   }
